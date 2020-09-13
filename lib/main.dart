52f3bc0bbe0c0e7a8d26866d8ba2b49186254d74
Prince Kaizen Namwali <33nano@pm.me>
2020-09-13 00:08:46 +0000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flame/flame.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intro_slider/intro_slider.dart';
+import 'package:spritewidget/spritewidget.dart';
+import 'package:assets_audio_player/assets_audio_player.dart';
+import 'package:circular_profile_avatar/circular_profile_avatar.dart';
+import 'package:flame_splash_screen/flame_splash_screen.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+// I'm still learning. Don't look surprised. Those Mockups did look cool though.😎
+// Start with the Flame splash screen.
+// App contains 7 routes from Main page
+//
